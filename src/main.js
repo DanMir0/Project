90b2c9c961776d3a9bf070b0bcbf837689ba6777
Dani <daniil.mironenko86@gmail.com>
2022-03-06 17:44:54 +0300

    Vuetifyjs

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "@/plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
 
-import Database from 'better-sqlite3';
-let db = new Database('./db/chinook.db', {verbose: console.log});
-console.log(db)
-const row = db.prepare('SELECT * FROM genres').all();
+import Database from "better-sqlite3";
+let db = new Database("./db/chinook.db", { verbose: console.log });
+console.log(db);
+const row = db.prepare("SELECT * FROM genres").all();
 console.log(row);
-db.prepare('INSERT INTO tab (id) VALUES (?)').run([1]);
+db.prepare("INSERT INTO tab (id) VALUES (?)").run([1]);
