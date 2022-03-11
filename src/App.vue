ebdd84671b1ba2570d9a7165a1b89a2f76ef1b3d
DanMir0 <daniil.mironenko86@gmail.com>
2022-03-12 00:50:32 +0800

    install router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app></v-app-bar>
+
+    <v-main>
+      <v-container> Hello World </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
