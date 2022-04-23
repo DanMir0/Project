d9420ad661aa044d6764110ca2db653a328ea514
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2022-04-23 19:06:09 +0300

    title Settings.vue

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,18 +1,33 @@
 <template>
-  <div>
-    <select-counterparties
-        v-model="settings.PRODUCTION_HALL"
-        :rules="[$rules.required]"
-        label="Производственный цех"
-    >
-    </select-counterparties>
-      <select-counterparties
-          v-model="settings.CUSTOMER_ID"
-          :rules="[$rules.required]"
-          label="Название организации"
-      >
-      </select-counterparties>
-    <button @click="save()">Сохранить</button>
+  <div class="settings">
+      <v-card>
+          <v-form>
+              <v-card-title>
+                  <span class="text-h5">Настройки</span>
+              </v-card-title>
+
+          <v-card-text>
+              <v-container>
+                  <select-counterparties
+                      v-model="settings.PRODUCTION_HALL"
+                      :rules="[$rules.required]"
+                      label="Производственный цех"
+                  >
+                  </select-counterparties>
+                  <select-counterparties
+                      v-model="settings.CUSTOMER_ID"
+                      :rules="[$rules.required]"
+                      label="Название организации"
+                  >
+                  </select-counterparties>
+              </v-container>
+          </v-card-text>
+              <v-card-actions>
+                  <v-spacer />
+                  <v-btn color="primary" @click="save">Сохранить</v-btn>
+              </v-card-actions>
+          </v-form>
+      </v-card>
   </div>
 </template>
 
@@ -36,7 +51,3 @@ export default {
 }
 
 </script>
-
-<style scoped>
-
-</style>
